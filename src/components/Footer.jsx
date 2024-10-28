9bb9f2b15099a159029f9380cd6eae827d86435a
Souvik Bera <souvikbera020@gmail.com>
2024-10-28 11:21:07 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+const Footer = () => (
+  <footer className="bg-gray-800 text-white text-center py-4">
+    <p>&copy; 2024 EventSpot Lite:  souvik </p>
+  </footer>
+);
+
+export default Footer;
